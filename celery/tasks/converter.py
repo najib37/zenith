6db4b368d758cadc92179a3c3bbec2b851c97e35
najib ramadan <najibramadan37@gmail.com>
2025-03-21 02:26:09 +0000

    separated the stream and converter logic, added the first steps of syncing the download and conversion

--- celery/tasks/converter.py
+++ celery/tasks/converter.py
@@ -1,21 +1,180 @@
 import asyncio
-import signal
-import os
 import json
+import os
+import signal
+from enum import Enum
 from time import sleep
+
 import ffmpeg
+
+
+class VideoResulotion(Enum):
+    SD = ("144", 256, 144, "1M")
+    HD = ("720", 1280, 720, "3M")
+    FHD = ("1080", 1920, 1080, "5M")
+
+    def __init__(
+        self,
+        prefix: str,
+        width: int,
+        height: int,
+        bitrate: str,
+    ):
+        self.prefix = prefix
+        self.width = width
+        self.height = height
+        self.bitrate = bitrate
+        # self.status = ConverterStatus.IDLE
+        self.segment = 0
+
+
+class ConverterStatus(Enum):
+    IDLE = "idle"
+    CONVERTING = "converting"
+    PAUSED = "paused"
+    DONE = "done"
+    DOWNLDED = "downloaded"
+
+    def __init__(self, status: str):
+        self.status = status
+
+
+class Stream:
+
+    def __init__(self, input_file, output_path):
+        self.status = ConverterStatus.IDLE
+        self.segment = 0
+        self.process = None
+        self.start_time = 0
+
+        self.input_file = input_file
+        self.output_path = output_path
+
+    def start(self, res: VideoResulotion):
+
+        if self.status == ConverterStatus.CONVERTING:
+            return
+        self.process = (
+            ffmpeg.input(self.input_file, ss=self.start_time)
+            .filter("scale", res.width, res.height)
+            .output(
+                f"{self.output_path}/{res.prefix}/{res.prefix}.m3u8",
+                format="hls",
+                hls_time=10,
+                hls_list_size=0,
+                hls_segment_filename=f"{self.output_path}/{res.prefix}/{res.prefix}_%03d.ts",
+                hls_flags="independent_segments",
+                start_number=0,
+                vcodec="libx264",
+                acodec="aac",
+                preset="fast",
+                tune="film",
+                crf=23,
+                threads=4,
+                video_bitrate=res.bitrate,
+                maxrate=res.bitrate,
+                bufsize="6M",
+                audio_bitrate="192k",
+                level="4.0",
+                map="0:a:0?",
+            )
+            .run_async()
+        )
+        self.status = ConverterStatus.CONVERTING
+
+    def init(self):
+        # TODO: create the palylist with zero segments
+        pass
+
+    def stop(self):
+        if self.status != ConverterStatus.CONVERTING:
+            return
+        if self.process and self.process.pid:
+            self.process.terminate()
+            try:
+                self.process.wait(timeout=10)
+            except TimeoutError:
+                print(f"Stream took too long to stop, forcing kill")
+                return
+            self.status = ConverterStatus.DONE
+
+    def pause(self):
+        print("_" * 100)
+        print("_" * 100)
+        print("_" * 100)
+        print("_" * 100)
+        print("attempting to pause")
+        print("_" * 100)
+        print("_" * 100)
+        print("_" * 100)
+        print("_" * 100)
+
+        print(f"Process: {self.process}")
+        print(f"Process status: {self.status}")
+
+        if self.status != ConverterStatus.CONVERTING:
+            return
+        if self.process and self.process.pid:
+            print("_" * 100)
+            print("_" * 100)
+            print("_" * 100)
+            print("_" * 100)
+            print("Killing process")
+            print("_" * 100)
+            print("_" * 100)
+            print("_" * 100)
+            print("_" * 100)
+            os.kill(self.process.pid, signal.SIGSTOP)
+        self.status = ConverterStatus.PAUSED
+
+    def resume(self):
+        if self.status != ConverterStatus.PAUSED:
+            return
+        if self.process and self.process.pid:
+            os.kill(self.process.pid, signal.SIGCONT)
+        self.status = ConverterStatus.CONVERTING
+
 
 class VideoConverter:
     def __init__(self, input_file, output_path, movie_key):
-        self.process = None
         self.streams = {}
         self.input_file = input_file
         self.output_path = f"{output_path}/{movie_key}"
-        self.is_started = False
-        self.current_segment = 0
         self.duration = self.get_video_duration(input_file)
+        self.status = ConverterStatus.IDLE
         os.makedirs(f"{self.output_path}/1080/", exist_ok=True)
         os.makedirs(f"{self.output_path}/720/", exist_ok=True)
+        os.makedirs(f"{self.output_path}/144/", exist_ok=True)
+
+    def start_conversion(self):
+        # self.create_playlist()
+
+        if (self.status == ConverterStatus.CONVERTING or not self.is_ready_to_convert()):
+            return
+
+        for res in VideoResulotion:
+            stream = Stream(self.input_file, self.output_path)
+            stream.start(res)
+            self.streams[res.prefix] = stream
+        self.status = ConverterStatus.CONVERTING
+
+    def pause_conversion(self):
+        print("Pause conversion")
+        for stream in self.streams.values():
+            stream.pause()
+        self.status = ConverterStatus.PAUSED
+
+    def resume_conversion(self):
+        print("Resume conversion")
+        for stream in self.streams.values():
+            stream.resume()
+        self.status = ConverterStatus.CONVERTING
+
+    def stop_conversion(self):
+        print("Stop conversion")
+        for stream in self.streams.values():
+            stream.stop()
+        self.status = ConverterStatus.DONE
 
     def file_exists(self, file_path):
         if not os.path.exists(self.input_file):
@@ -23,13 +182,18 @@ class VideoConverter:
         return os.path.isfile(file_path) and os.access(file_path, os.R_OK)
 
     def is_ready_to_convert(self):
+
+        if self.duration == 0:
+            self.duration = self.get_video_duration(self.input_file)
+
         return self.file_exists(self.input_file) and self.duration > 0
 
     def get_video_duration(self, file_path):
-        if not self.file_exists(file_path): return 0
+        if not self.file_exists(file_path):
+            return 0
         try:
             probe = ffmpeg.probe(file_path)
-            duration = float(probe['format']['duration'])
+            duration = float(probe["format"]["duration"])
             print("----------------------------")
             print(f"Duration: {duration}")
             print("----------------------------")
@@ -38,115 +202,45 @@ class VideoConverter:
             return 0
 
     def create_playlist(self):
-        with open(f"{self.output_path}/master.m3u8", 'w') as f:
+        with open(f"{self.output_path}/master.m3u8", "w") as f:
             f.write(
                 "#EXTM3U\n"
                 "#EXT-X-VERSION:3\n"
                 "#EXT-X-STREAM-INF:BANDWIDTH=5000000,RESOLUTION=1920x1080\n"
-                "1080.m3u8\n"
+                "1080/1080.m3u8\n"
                 "#EXT-X-STREAM-INF:BANDWIDTH=3000000,RESOLUTION=1280x720\n"
-                "720.m3u8\n"
+                "720/720.m3u8\n"
                 "#EXT-X-STREAM-INF:BANDWIDTH=1000000,RESOLUTION=256x144\n"
-                "144.m3u8\n"
+                "144/144.m3u8\n"
             )
 
-    def start_conversion(self):
-        start_time = 0
-
-        self.streams['sd'] = (
-            ffmpeg
-            .input(self.input_file, ss=start_time)
-            .filter('scale', 256, 144)
-            .output(
-                f"{self.output_path}/144.m3u8",
-                format='hls',
-                hls_time=5,
-                hls_list_size=0,
-                hls_segment_filename=f"{self.output_path}/144_%03d.ts",
-                hls_flags='independent_segments',
-                start_number=self.current_segment,
-                vcodec='libx264',
-                acodec='aac',
-                preset='fast',
-                tune='film',
-                crf=23,
-                threads=4,
-
-                video_bitrate='3M',
-                maxrate='3M',
-                bufsize='6M',
-                audio_bitrate='192k',
-                level='4.0',
-            )
-            .run_async()
-        ) 
-
-        self.streams['fhd'] = (
-            ffmpeg
-            .input(self.input_file, ss=start_time)
-            .filter('scale', 1920, 1080)
-            .output(
-                f"{self.output_path}/1080.m3u8",
-                format='hls',
-                hls_time=5,
-                hls_list_size=0,
-                hls_segment_filename=f"{self.output_path}/1080_%03d.ts",
-                hls_flags='independent_segments',
-                start_number=self.current_segment,
-                vcodec='libx264',
-                acodec='aac',
-                preset='fast',
-                tune='film',
-                crf=23,
-                threads=4,
-
-                video_bitrate='3M',
-                maxrate='3M',
-                bufsize='6M',
-                audio_bitrate='192k',
-                level='4.0',
-            )
-            .run_async()
-        )
-        self.streams['hd'] = (
-            ffmpeg
-            .input(self.input_file, ss=start_time)
-            .filter('scale', 1280, 720)
-            .output(
-                f"{self.output_path}/720.m3u8",
-                format='hls',
-                hls_time=5,
-                hls_list_size=0,
-                hls_segment_filename=f"{self.output_path}/720_%03d.ts",
-                hls_flags='independent_segments',
-                start_number=self.current_segment,
-                vcodec='libx264',
-                acodec='aac',
-                preset='fast',
-                tune='film',
-                crf=23,
-                threads=4,
-
-                video_bitrate='3M',
-                maxrate='3M',
-                bufsize='6M',
-                audio_bitrate='192k',
-                level='4.0',
-            )
-            .run_async()
-        ) 
-        self.create_playlist()
-
-        self.is_started = True
-
-        print(f"Conversion started from segment {self.current_segment}. Press Ctrl+C to pause/resume.")
 
 def main():
     converter = VideoConverter(
         input_file="/home/data/hls/na.mkv",
         output_path="/home/data/hls/najib",
-        movie_key=55
+        movie_key=55,
     )
+    converter.start_conversion()
+
+    sleep(10)
+
+    print("*" * 100)
+    print("*" * 100)
+    converter.pause_conversion()
+
+    sleep(10)
+
+    print("*" * 100)
+    print("*" * 100)
+    converter.resume_conversion()
+
+    sleep(10)
+
+    print("*" * 100)
+    print("*" * 100)
+    converter.stop_conversion()
+
 
 if __name__ == "__main__":
     main()
--- celery/tasks/download_torrents.py
+++ celery/tasks/download_torrents.py
@@ -1,40 +1,43 @@
+import mimetypes
 from time import sleep
-from celery.signals import worker_process_init
-from celery_singleton import Singleton
-from celery import Celery
+
 # from celery.utils import time
 import libtorrent as lt
-import mimetypes
-from tasks.converter import VideoConverter
+from celery.signals import worker_process_init, worker_shutdown
+from celery_singleton import Singleton
+from tasks.converter import ConverterStatus, VideoConverter, VideoResulotion
+
+from celery import Celery
 
 app = Celery(
-    'tasks',
-    broker='amqp://userf:userd@rabbitmq:5672',
-    backend='redis://redis:6379/0',
-    task_serializer='json',
-    accept_content=['json'],
-    result_serializer='json',
-    timezone='UTC',
+    "tasks",
+    broker="amqp://userf:userd@rabbitmq:5672",
+    backend="redis://redis:6379/0",
+    task_serializer="json",
+    accept_content=["json"],
+    result_serializer="json",
+    timezone="UTC",
     # singleton_backend_url='redis://redis:6379/0',
     enable_utc=True,
 )
 
+
 class TorrentDownloader:
 
     _instance = None
-    
+
     @classmethod
     def get_instance(cls):
         if cls._instance is None:
             cls._instance = cls()
         return cls._instance
-    
+
     def __init__(self):
         if TorrentDownloader._instance is not None:
             raise Exception("This class is a singleton! Use get_instance() instead")
         self.session = lt.session()
-        self.session.add_extension('ut_metadata')
-        self.session.add_extension('ut_pex')
+        self.session.add_extension("ut_metadata")
+        self.session.add_extension("ut_pex")
 
         self.jobs = {}
 
@@ -47,7 +50,8 @@ class TorrentDownloader:
             "num_peers": handle.status().num_peers,
             "num_seeds": handle.status().num_seeds,
             "total_size": handle.status().total_wanted / (1024 * 1024),  # Convert to MB
-            "total_downloaded": handle.status().total_done / (1024 * 1024),  # Convert to MB
+            "total_downloaded": handle.status().total_done
+            / (1024 * 1024),  # Convert to MB
             "state": str(handle.status().state),
             "paused": handle.status().paused,
             "is_finished": handle.status().is_finished,
@@ -60,7 +64,7 @@ class TorrentDownloader:
             return {
                 "status": "torrent_already_added",
                 "code": 200,
-                "info": self.get_info_from_handle(self.jobs[movie_key]['handle']),
+                "info": self.get_info_from_handle(self.jobs[movie_key]["handle"]),
             }
 
         relative_path = f"/movies/{str(movie_key)}/"
@@ -71,29 +75,36 @@ class TorrentDownloader:
         handle = self.session.add_torrent(params)
         handle.set_sequential_download(True)
         mimetypes.init()
-        mimetypes.add_type('video/x-matroska', '.mkv')
-        mimetypes.add_type('video/mp4', '.mp4')
+        mimetypes.add_type("video/x-matroska", ".mkv")
+        mimetypes.add_type("video/mp4", ".mp4")
 
         time = 0
         while not handle.status().has_metadata:
-            if time > 10: return {
-                "status": "no_metadata",
-                "code": 404
-            }
+            if time > 10:
+                return {"status": "no_metadata", "code": 404}
             # TODO: add cleanup and use celery sleep
             sleep(0.5)
             time += 0.5
 
+
+
         files = [
             {
                 "path": f"{full_path}/{f.path}",
+                # "path": "/home/data/hls/na.mkv",
                 "size": f.size,
-                "type": mimetypes.guess_type(f.path)[0]
+                "type": mimetypes.guess_type(f.path)[0],
             }
             for f in handle.status().torrent_file.files()
         ]
 
-        movie_info = max(files, key=lambda x: x['size'])
+        movie_info = max(files, key=lambda x: x["size"])
+
+        print("+=" * 100)
+        print("+=" * 100)
+        print(f"{full_path}/{movie_info.get('path')}")
+        print("+=" * 100)
+        print("+=" * 100)
 
         converter = self.post_process(movie_key, movie_info)
 
@@ -115,16 +126,20 @@ class TorrentDownloader:
         return formated_metadata
 
     def post_process(self, movie_key, movie_info):
-        match str(movie_info['type']):
-            case 'video/x-matroska':
-                converter = VideoConverter(movie_info['path'], '/home/data/movies/outs', movie_key)
+        match str(movie_info["type"]):
+            case "video/x-matroska":
+                converter = VideoConverter(
+                    movie_info["path"], "/home/data/movies/outs", movie_key
+                )
                 return converter
-            case 'video/mp4':
+            case "video/mp4":
                 pass
             case _:
                 pass
+
     def get_jobs(self):
         return self.jobs
+
 
 @worker_process_init.connect
 def initialize_torrent_session(**kwargs):
@@ -133,51 +148,79 @@ def initialize_torrent_session(**kwargs):
     print("Initializing torrent session")
     torrent_downloader = TorrentDownloader.get_instance()
 
+@worker_shutdown.connect
+def cleanup_on_shutdown(**kwargs):
+    """Perform cleanup when worker shuts down"""
+    global torrent_downloader
+    if torrent_downloader:
+        torrent_downloader.session.pause()
+        for _, job in torrent_downloader.jobs.items():
+            handler, converter = job.values()
+            converter.stop_conversion()
+            handler.pause()
+
 @app.task(
-    name='download_torrents',
-    queue='torrent_queue',
+    name="download_torrents",
+    queue="torrent_queue",
 )
 def download_torrents(magnet_link, movie_key):
     global torrent_downloader
     info = torrent_downloader.add_torrent(magnet_link, movie_key)
     return info
 
+
 @app.task(
-    name='process_jobs',
+    name="process_jobs",
     base=Singleton,
-    queue='torrent_queue',
-    # lock_expiry=60, 
+    queue="torrent_queue",
+    # lock_expiry=60,
     # raise_on_duplicate=False
 )
 def process_jobs():
     a = 0
     torrent_downloader = TorrentDownloader.get_instance()
+
     while len(torrent_downloader.jobs) > 0:
-        print("+++++++++++++++++++++++++++++++++++")
-        print("dora dora dora")
-        print("+++++++++++++++++++++++++++++++++++")
-        for _, job in torrent_downloader.jobs.items():
+        print("dora ---------------------------------")
+
+        # return {}
+
+        for key, job in torrent_downloader.jobs.items():
             handler, converter = job.values()
-            print("+++++++++++++++++++++++++++++++++++")
-            print(f"progress {handler.status().progress}")
-            print("+++++++++++++++++++++++++++++++++++")
-            # if not converter.is_started and converter.is_ready_to_convert():
-            #     print("+++++++++++++++++++++++++++++++++++")
-            #     print("Starting conversion")
-            #     print("+++++++++++++++++++++++++++++++++++")
-            #     converter.start_conversion()
-            #     # converter.create_playlist()
-            #     converter.is_stated = True
-            # if a == 0:
-            #     job['converter'].start_conversion()
+
+            # full downloaded and converted job completed
+            if handler.status().is_finished and converter.status != ConverterStatus.DONE:
+                converter.stop_conversion()
+                del torrent_downloader.jobs[key]
+                continue
+
+            # init conversion
+            if handler.status().downloading and converter.status == ConverterStatus.IDLE:
+                print("_" * 100)
+                print("_" * 100)
+                print("Start conversion")
+                print("_" * 100)
+                print("_" * 100)
+                converter.start_conversion()
+
+            # full downloaded but not converted
+            if handler.status().is_finished and converter.status != ConverterStatus.CONVERTING:
+                continue
+
+            # download paused pause conversion 
+            if handler.status().paused and converter.status == ConverterStatus.CONVERTING:
+                converter.pause_conversion()
+
         # a += 1
-        # if a > 10: break
+        # if a > 10:
+        #     break
         sleep(2)
 
     return {
         "status": "done",
         "code": 200,
     }
+
 
 # @app.task(name='get_torrent_info')
 # def get_torrent_info(movie_key):
